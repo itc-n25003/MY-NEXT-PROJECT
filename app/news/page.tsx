bd76cec9e0acb6301207ef4819a87c519af37dfc
itc-n25003 <n25003@std.it-college.ac.jp>
2026-01-22 10:50:21 +0900

    6章終了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,7 @@
-import { getNewsList } from "../_libs/microcms";
+import { getNewsList } from "@/app/_libs/microcms";
+import NewsList from "@/app/_components/NewsList";
 
 export default async function Page() {
   const { contents: news } = await getNewsList();
-
-  return <div>{JSON.stringify(news)}</div>;
+  return <NewsList news={news} />;
 }
